b9d68998504c68564e5a116119f128e74487ee42
da Silva, Wanderson <wanderson.dasilva@pearson.com>
2021-06-20 17:11:10 -0300

    Fixing heroku connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] ='stark-shore-42216.herokuapp.com'
-app.config['PREFERRED_URL_SCHEME'] ='https'
 
 from app.controllers import default
 from app.controllers import return_of_investiment
